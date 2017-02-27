199b4342842555405218862c96b6680a6299213f
Michel Boucey <michel.boucey@cybervisible.fr>
2017-02-27 14:30:14 +0100

    Update copyright

--- Text/IPv6Addr/Types.hs
+++ Text/IPv6Addr/Types.hs
@@ -8,10 +8,10 @@ instance Show IPv6Addr where
   show (IPv6Addr a) = T.unpack a
 
 data IPv6AddrToken
-  = SixteenBit !T.Text  -- ^ A four hexadecimal digits group representing a 16-Bit chunk
+  = SixteenBit  !T.Text -- ^ A four hexadecimal digits group representing a 16-Bit chunk
   | AllZeros            -- ^ An all zeros 16-Bit chunk
   | Colon               -- ^ A separator between 16-Bit chunks
   | DoubleColon         -- ^ A double-colon stands for a unique compression of many consecutive 16-Bit chunks
-  | IPv4Addr !T.Text    -- ^ An embedded IPv4 address as representation of the last 32-Bit
+  | IPv4Addr    !T.Text -- ^ An embedded IPv4 address as representation of the last 32-Bit
   deriving (Eq, Show)
 
